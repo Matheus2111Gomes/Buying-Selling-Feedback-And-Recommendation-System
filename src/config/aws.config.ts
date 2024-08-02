2856c0e00ed2cb1e3a16f40414eff0342e0961c7
MatheusGomes <matheus2111gomes@gmail.com>
2024-08-02 16:31:07 -0300

    adding a auth module and aws credential config

---
+++ src/config/aws.config.ts
@@ -0,0 +1,6 @@
+// src/config/aws.config.ts
+export const awsConfig = {
+    region: 'your-region',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  };
