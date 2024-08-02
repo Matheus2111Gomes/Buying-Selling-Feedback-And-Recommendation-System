d4a38ea7b63b30e340a590b9b361972865af3332
MatheusGomes <matheus2111gomes@gmail.com>
2024-08-02 16:46:46 -0300

    adding a feedback module and a serverless yml file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { FeedbackModule } from './feedback/feedback.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, FeedbackModule],
   controllers: [AppController],
   providers: [AppService],
 })
